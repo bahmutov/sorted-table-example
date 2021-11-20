717eb0167ceb17bd7afcca4e54ca09646169f6a1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-11-20 15:47:30 -0500

    add badges

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,10 +1,13 @@
 /// <reference types="cypress" />
 
+// https://www.chaijs.com/plugins/chai-sorted/
+chai.use(require('chai-sorted'))
+
 beforeEach(() => {
   cy.visit('app/table.html')
 })
 
-it('is not sorted at first', () => {
+it('is not sorted at first (1)', () => {
   cy.get('tbody td:nth-child(2)')
     .should('have.length', 4)
     .then(($cells) => Cypress._.map($cells, 'innerText'))
@@ -16,4 +19,14 @@ it('is not sorted at first', () => {
       const sorted = Cypress._.sortBy(timestamps)
       expect(timestamps).to.not.deep.equal(sorted)
     })
+})
+
+it.only('is not sorted at first', () => {
+  cy.get('tbody td:nth-child(2)')
+    .should('have.length', 4)
+    .then(($cells) => Cypress._.map($cells, 'innerText'))
+    // .then(($cells) => $cells.text())
+    .then((strings) => Cypress._.map(strings, (s) => new Date(s)))
+    .then((dates) => Cypress._.map(dates, (d) => d.getTime()))
+    .should('not.be.sorted')
 })

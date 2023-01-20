b219c4acb430999c56dcf603dc356adb167223cb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-01-20 11:51:14 -0500

    solutions

--- cypress/e2e/table.cy.js
+++ cypress/e2e/table.cy.js
@@ -6,16 +6,19 @@ import 'cypress-map'
 // https://www.chaijs.com/plugins/chai-sorted/
 chai.use(require('chai-sorted'))
 
-it('confirms the headings', () => {
+beforeEach(() => {
   cy.visit('app/table.html')
-  cy.get('table thead')
-    .table()
-    .its(0)
-    .should('deep.equal', ['Name', 'Date (YYYY-MM-DD)', 'Age'])
+})
+
+it('confirms the headings', () => {
+  // can you confirm the table has the following headings?
+  const headings = ['Name', 'Date (YYYY-MM-DD)', 'Age']
+  cy.get('table thead').table().its(0).should('deep.equal', headings)
 })
 
 it('confirms the sorted age column', () => {
-  cy.visit('app/table.html')
+  // can you confirm the "Age" column is not sorted initially?
+  // Tip: Use cy.table and other queries from cypress-map
   cy.get('table tbody')
     .table(2, 0, 1)
     .print()
@@ -23,7 +26,9 @@ it('confirms the sorted age column', () => {
     .print('merged %o')
     .map(Number)
     .should('not.be.sorted')
+  // click the sort button
   cy.get('#sort-by-date').click()
+  // the "Age" column should be sorted in ascending order
   cy.get('table tbody')
     .table(2, 0, 1)
     .print()
@@ -31,4 +36,26 @@ it('confirms the sorted age column', () => {
     .print('merged %o')
     .map(Number)
     .should('be.ascending')
+  // Bonus: there is a lot of duplication in the above test
+  // Can you simplify it?
+})
+
+it('confirms the name and dates of the last two sorted rows', () => {
+  // sort the table by date
+  cy.get('#sort-by-date').click()
+  // confirm the last two rows have
+  // the following name and dates
+  const values = [
+    ['Joe', '2001-01-24'],
+    ['Anna', '2010-03-26'],
+  ]
+  cy.get('table tbody').table(0, 2, 2, 2).print().should('deep.equal', values)
+  // what if we don't know the number of rows?
+  // Can you rewrite the above code to slice the table correctly?
+  cy.get('table tbody')
+    .table()
+    .invoke('slice', -2)
+    .mapInvoke('slice', 0, 2)
+    .print()
+    .should('deep.equal', values)
 })
